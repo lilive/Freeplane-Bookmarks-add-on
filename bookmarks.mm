1451c1636f83a77d51d83bae4bab3602e41570d6
lilive <lilivounet@gmail.com>
2019-07-07 21:17:32 +0200

    feat: add a tool to update bookmarks from previous version

--- bookmarks.mm
+++ bookmarks.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.7.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="Bookmarks" LOCALIZED_STYLE_REF="styles.topic" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1562500393682" LINK="https://github.com/lilive/Freeplane-Bookmarks-add-on" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#000000" STYLE="rectangle" SHAPE_HORIZONTAL_MARGIN="6.0 pt" SHAPE_VERTICAL_MARGIN="6.0 pt">
+<node TEXT="Bookmarks" LOCALIZED_STYLE_REF="styles.topic" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1562511617909" LINK="https://github.com/lilive/Freeplane-Bookmarks-add-on" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#000000" STYLE="rectangle" SHAPE_HORIZONTAL_MARGIN="6.0 pt" SHAPE_VERTICAL_MARGIN="6.0 pt">
 <edge WIDTH="thin"/>
 <hook NAME="MapStyle" background="#c4c4c4">
     <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" show_notes_in_map="false" MarksKeys="{&quot;108&quot;:&quot;ID_303460997&quot;,&quot;99&quot;:&quot;ID_1257390437&quot;,&quot;111&quot;:&quot;ID_185824507&quot;}" edgeColorConfiguration="#353535ff,#353535ff,#353535ff,#353535ff,#353535ff" Marks_stored_node_id="ID_1837714869"/>
@@ -131,9 +131,8 @@
     </ul>
   </body>
 </html>
-
 </richcontent>
-<node TEXT="description" POSITION="left" ID="ID_1114811084" CREATED="1562410359487" MODIFIED="1562499736245" TEXT_SHORTENED="true">
+<node TEXT="description" POSITION="left" ID="ID_1114811084" CREATED="1562410359487" MODIFIED="1562511617937" TEXT_SHORTENED="true">
 <edge COLOR="#353535"/>
 <richcontent TYPE="NOTE">
 
@@ -153,11 +152,10 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 <node TEXT="Add bookmarks to nodes, and easily navigate beetween them." ID="ID_1183587619" CREATED="1518707454288" MODIFIED="1518815727777"/>
 </node>
-<node TEXT="changes" POSITION="left" ID="ID_1094494132" CREATED="1562410359592" MODIFIED="1562499736248" TEXT_SHORTENED="true">
+<node TEXT="changes" POSITION="left" ID="ID_1094494132" CREATED="1562410359592" MODIFIED="1562511617940" TEXT_SHORTENED="true">
 <edge COLOR="#353535"/>
 <richcontent TYPE="NOTE">
 
@@ -171,7 +169,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 <node TEXT="v0.3" ID="ID_1947541306" CREATED="1518881580800" MODIFIED="1518881585971">
 <node TEXT="First released version" ID="ID_1150257103" CREATED="1518881587391" MODIFIED="1518881596914"/>
@@ -192,7 +189,7 @@
 <node TEXT="Rename bookmarks icons" ID="ID_27269396" CREATED="1562499884480" MODIFIED="1562499895702"/>
 </node>
 </node>
-<node TEXT="license" FOLDED="true" POSITION="left" ID="ID_230785910" CREATED="1562410359599" MODIFIED="1562499736251" TEXT_SHORTENED="true">
+<node TEXT="license" FOLDED="true" POSITION="left" ID="ID_230785910" CREATED="1562410359599" MODIFIED="1562511617945" TEXT_SHORTENED="true">
 <edge COLOR="#353535"/>
 <richcontent TYPE="NOTE">
 
@@ -212,11 +209,10 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 <node TEXT="&#xa;This add-on is free software: you can redistribute it and/or modify&#xa;it under the terms of the GNU General Public License as published by&#xa;the Free Software Foundation, either version 2 of the License, or&#xa;(at your option) any later version.&#xa;&#xa;This program is distributed in the hope that it will be useful,&#xa;but WITHOUT ANY WARRANTY; without even the implied warranty of&#xa;MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the&#xa;GNU General Public License for more details.&#xa;" ID="ID_1167386088" CREATED="1562410359607" MODIFIED="1562410359614"/>
 </node>
-<node TEXT="preferences.xml" POSITION="left" ID="ID_1098616655" CREATED="1562410359628" MODIFIED="1562499736256" TEXT_SHORTENED="true">
+<node TEXT="preferences.xml" POSITION="left" ID="ID_1098616655" CREATED="1562410359628" MODIFIED="1562511617956" TEXT_SHORTENED="true">
 <edge COLOR="#353535"/>
 <richcontent TYPE="NOTE">
 
@@ -233,10 +229,9 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
-<node TEXT="default.properties" POSITION="left" ID="ID_151630546" CREATED="1562410359652" MODIFIED="1562499736259" TEXT_SHORTENED="true">
+<node TEXT="default.properties" POSITION="left" ID="ID_151630546" CREATED="1562410359652" MODIFIED="1562511617963" TEXT_SHORTENED="true">
 <edge COLOR="#353535"/>
 <richcontent TYPE="NOTE">
 
@@ -258,10 +253,9 @@
     </ul>
   </body>
 </html>
-
 </richcontent>
 </node>
-<node TEXT="translations" POSITION="left" ID="ID_255907585" CREATED="1562410359658" MODIFIED="1562499736262" TEXT_SHORTENED="true">
+<node TEXT="translations" POSITION="left" ID="ID_255907585" CREATED="1562410359658" MODIFIED="1562511617966" TEXT_SHORTENED="true">
 <edge COLOR="#353535"/>
 <richcontent TYPE="NOTE">
 
@@ -286,9 +280,8 @@
     </ul>
   </body>
 </html>
-
 </richcontent>
-<node TEXT="en" ID="ID_737947027" CREATED="1562410359665" MODIFIED="1562437580849">
+<node TEXT="en" ID="ID_737947027" CREATED="1562410359665" MODIFIED="1562516776028">
 <attribute_layout NAME_WIDTH="170.999994903803 pt" VALUE_WIDTH="320.9999904334548 pt"/>
 <attribute NAME="addons.${name}" VALUE="Bookmarks"/>
 <attribute NAME="addons.${name}.BookmarkNode" VALUE="Add / Remove a bookmark"/>
@@ -296,8 +289,10 @@
 <attribute NAME="addons.${name}.ToggleBookmark" VALUE="Toggle the bookmark"/>
 <attribute NAME="addons.${name}.CreateLinkInBookmark" VALUE="Create link in bookmarked node"/>
 <attribute NAME="addons.${name}.CreateLinkToBookmark" VALUE="Create link to bookmarked node"/>
+<attribute NAME="addons.${name}.UpdateMap" VALUE="Update map with bookmarks defined by a previous version of the addon"/>
+<attribute NAME="tools" VALUE="Tools"/>
 </node>
-<node TEXT="fr" ID="ID_1748679509" CREATED="1562410359665" MODIFIED="1562437585762">
+<node TEXT="fr" ID="ID_1748679509" CREATED="1562410359665" MODIFIED="1562516867944">
 <attribute_layout NAME_WIDTH="170.24999492615476 pt" VALUE_WIDTH="320.9999904334548 pt"/>
 <attribute NAME="addons.${name}" VALUE="Marque-pages"/>
 <attribute NAME="addons.${name}.BookmarkNode" VALUE="Placer/Supprimer un marque-page"/>
@@ -305,9 +300,11 @@
 <attribute NAME="addons.${name}.ToggleBookmark" VALUE="Basculer un marque-page"/>
 <attribute NAME="addons.${name}.CreateLinkInBookmark" VALUE="Cr\u00E9er un lien dans un marque-page"/>
 <attribute NAME="addons.${name}.CreateLinkToBookmark" VALUE="Cr\u00E9er un lien vers un marque-page"/>
+<attribute NAME="addons.${name}.UpdateMap" VALUE="Mettre \u00E0 jour les marque-pages d&apos;une version ant\u00E9rieure du module"/>
+<attribute NAME="tools" VALUE="Outils"/>
 </node>
 </node>
-<node TEXT="deinstall" POSITION="left" ID="ID_1870605844" CREATED="1562410359689" MODIFIED="1562499790576">
+<node TEXT="deinstall" POSITION="left" ID="ID_1870605844" CREATED="1562410359689" MODIFIED="1562511617974">
 <edge COLOR="#353535"/>
 <richcontent TYPE="NOTE">
 
@@ -321,7 +318,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 <attribute_layout VALUE_WIDTH="389.9999883770946 pt"/>
 <attribute NAME="delete" VALUE="${installationbase}/addons/${name}.script.xml"/>
@@ -337,8 +333,9 @@
 <attribute NAME="delete" VALUE="${installationbase}/addons/${name}/scripts/Common.groovy"/>
 <attribute NAME="delete" VALUE="${installationbase}/icons/bookmarks/Bookmark 1.svg"/>
 <attribute NAME="delete" VALUE="${installationbase}/icons/bookmarks/Bookmark 2.svg"/>
+<attribute NAME="delete" VALUE="${installationbase}/addons/${name}/scripts/UpdateMapTo_V_0_5_1.groovy"/>
 </node>
-<node TEXT="scripts" POSITION="right" ID="ID_1460492722" CREATED="1562410359701" MODIFIED="1562499736279" TEXT_SHORTENED="true">
+<node TEXT="scripts" POSITION="right" ID="ID_1460492722" CREATED="1562410359701" MODIFIED="1562511617981" TEXT_SHORTENED="true">
 <edge COLOR="#353535"/>
 <richcontent TYPE="NOTE">
 
@@ -457,7 +454,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 <node TEXT="BookmarkNode.groovy" ID="ID_1722187040" CREATED="1562410359729" MODIFIED="1562412132309">
 <attribute_layout NAME_WIDTH="172.49999485909953 pt" VALUE_WIDTH="213.7499936297538 pt"/>
@@ -519,8 +515,20 @@
 <attribute NAME="execute_scripts_without_exec_restriction" VALUE="false"/>
 <attribute NAME="execute_scripts_without_network_restriction" VALUE="false"/>
 </node>
+<node TEXT="UpdateMap.groovy" ID="ID_1665379419" CREATED="1562511659226" MODIFIED="1562516491246">
+<attribute_layout NAME_WIDTH="183.74999452382343 pt" VALUE_WIDTH="239.24999286979457 pt"/>
+<attribute NAME="menuTitleKey" VALUE="addons.${name}.UpdateMap"/>
+<attribute NAME="menuLocation" VALUE="/menu_bar/${name}/tools"/>
+<attribute NAME="executionMode" VALUE="on_single_node"/>
+<attribute NAME="keyboardShortcut" VALUE=""/>
+<attribute NAME="execute_scripts_without_asking" VALUE="true"/>
+<attribute NAME="execute_scripts_without_file_restriction" VALUE="true"/>
+<attribute NAME="execute_scripts_without_write_restriction" VALUE="false"/>
+<attribute NAME="execute_scripts_without_exec_restriction" VALUE="false"/>
+<attribute NAME="execute_scripts_without_network_restriction" VALUE="false"/>
 </node>
-<node TEXT="lib" POSITION="right" ID="ID_1159449610" CREATED="1562410359919" MODIFIED="1562499736338">
+</node>
+<node TEXT="lib" POSITION="right" ID="ID_1159449610" CREATED="1562410359919" MODIFIED="1562511618045">
 <edge COLOR="#353535"/>
 <richcontent TYPE="NOTE">
 
@@ -558,10 +566,9 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
-<node TEXT="zips" POSITION="right" ID="ID_435012533" CREATED="1562410359928" MODIFIED="1562499736343">
+<node TEXT="zips" POSITION="right" ID="ID_435012533" CREATED="1562410359928" MODIFIED="1562511618049">
 <edge COLOR="#353535"/>
 <richcontent TYPE="NOTE">
 
@@ -599,12 +606,11 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 <node TEXT="icons" ID="ID_1144883898" CREATED="1562410359939" MODIFIED="1562410359940"/>
 <node TEXT="scripts" ID="ID_87188883" CREATED="1562410359941" MODIFIED="1562410359942"/>
 </node>
-<node TEXT="images" POSITION="right" ID="ID_63266556" CREATED="1562410359950" MODIFIED="1562499736369" TEXT_SHORTENED="true">
+<node TEXT="images" POSITION="right" ID="ID_63266556" CREATED="1562410359950" MODIFIED="1562511618076" TEXT_SHORTENED="true">
 <edge COLOR="#353535"/>
 <richcontent TYPE="NOTE">
 
@@ -638,7 +644,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 </node>
 </node>
---
+++ scripts/UpdateMap.groovy
@@ -0,0 +1,105 @@
+// Update the bookmarks if they where defined by a previous version of the addon
+
+import org.freeplane.plugin.script.proxy.Convertible
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+
+storageKey = "BookmarksKeys"
+anonymousIcon = "bookmarks/Bookmark 1"
+namedIcon = "bookmarks/Bookmark 2"
+
+def Map loadOldNamedBookmarks()
+{
+    // Load the named bookmarks stored in the "MarksKeys" entry in map local storage
+    def marksString = new Convertible( '{}' )
+    def stored = node.map.storage.getAt( "MarksKeys" )
+    if( stored ) marksString = stored;
+    def namedBookmarks = new JsonSlurper().parseText( marksString.getText() )
+    return namedBookmarks as Map
+}
+
+def Map loadNamedBookmarks()
+{
+    // Load the named bookmarks stored in the storageKey entry in map local storage
+    def marksString = new Convertible( '{}' )
+    def stored = node.map.storage.getAt( storageKey )
+    if( stored ) marksString = stored;
+    def namedBookmarks = new JsonSlurper().parseText( marksString.getText() )
+    return namedBookmarks as Map
+}
+
+def deleteOldNamedBookmarksDatas()
+{
+    // Delete the bookmarks stored in the "MarksKeys" entry in map local storage
+    node.map.storage.putAt( "MarksKeys", null )
+}
+
+def mergeNamedBookmarks( oldBookmarks, uptodateBookmarks )
+{
+    // Merge the old bookmarks in the already up to date bookmarks
+
+    conflicts = [:]
+    oldBookmarks.each
+    {
+        key, id ->
+        if( uptodateBookmarks.containsKey( key ) ) conflicts[ key ] = uptodateBookmarks[ key ]
+        uptodateBookmarks[ key ] = id;
+    }
+    return [ uptodateBookmarks, conflicts ]
+}
+
+def saveNamedBookmarks( namedBookmarks )
+{
+    // Save the named bookmarks to the storageKey entry in map local storage
+    def builder = new JsonBuilder()
+    builder( namedBookmarks )
+    node.map.storage.putAt( storageKey, builder.toString() )
+}
+
+// Look for named bookmarks in the local map storage, to find them as defined
+// by previous version of the addon
+oldNamedBookmarks = loadOldNamedBookmarks()
+if( oldNamedBookmarks )
+{
+    // Delete them from the local storage
+    deleteOldNamedBookmarksDatas()
+    
+    // Get already updated bookmarks
+    namedBookmarks = loadNamedBookmarks()
+    
+    // Merge the two
+    ( namedBookmarks, conflicts ) = mergeNamedBookmarks( oldNamedBookmarks, namedBookmarks )
+    
+    // Save them with the new settings
+    saveNamedBookmarks( namedBookmarks )
+    
+    // If an already uptodate bookmark as the same key than an old one,
+    // convert this bookmark to anonymous bookmark
+    conflicts.each
+    {
+        key, id ->
+        def n = map.node( id )
+        if( n )
+        {
+            n.icons.remove( namedIcon )
+            n.icons.add( anonymousIcon )
+        }
+    }
+}
+
+// Now replace the old bookmarks icons names
+c.findAll().each
+{
+    def update = false
+    while( it.icons.remove( "bookmark-named" ) ) update = true
+    if( update )
+    {
+        it.icons.add( namedIcon )
+        return
+    }
+    update = false
+    while( it.icons.remove( "bookmark" ) ) update = true
+    if( update ) it.icons.add( anonymousIcon )
+}
+
+
